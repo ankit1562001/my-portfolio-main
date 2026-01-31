f955be8a3bc696d2ca3f7ef5700c22a5928f3a9c
Ankit  kumar Prajapati <ankitprajapati4433@gmail.com>
2026-01-31 10:54:14 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Vite configuration for React application
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
